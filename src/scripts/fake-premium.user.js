9d4e427c4eaeda9eb75b87a168751ad7c2cb62f6
Asger Finding <asger.finding@gmail.com>
2024-01-06 20:07:04 +0100

    feat: add fake premium userscript

---
+++ src/scripts/fake-premium.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        Fake Premium
+// @author      commander
+// @description Makes the game think you have premium (only visual)
+// @namespace   https://github.com/asger-finding
+// @version     0.0.1
+// @license     GPL-3.0
+// @match       *://*.tanktrouble.com/*
+// @run-at      document-end
+// @grant       none
+// @noframes
+// ==/UserScript==
+
+const { _updatePremium } = PremiumManager;
+PremiumManager.classFields({
+	get _updatePremium() {
+		_updatePremium(true);
+		return () => {};
+	},
+	set _updatePremium(value) {
+		return value;
+	}
+});
